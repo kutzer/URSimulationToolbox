8adb009ed0d7adee5baeb2c8373e1ed830b1de16
Michael Kutzer <kutzer@usna.edu>
2025-05-23 12:32:26 -0400

    Update URSimulationToolboxUpdate.m

--- URSimulationToolboxFunctions/URSimulationToolboxUpdate.m
+++ URSimulationToolboxFunctions/URSimulationToolboxUpdate.m
@@ -6,21 +6,23 @@ function URSimulationToolboxUpdate
 
 % Updates:
 %   26Mar2021 - Accounted for GitHub's removal of "master" in references.
+%   22May2025 - Updated to enable local install
 
-% TODO - Find a location for "URSimulationToolbox Example SCRIPTS"
 % TODO - update function for general operation
 
 ToolboxUpdate('URSimulation');
 
 end
 
+%% Internal functions (unique workspace)
+% ------------------------------------------------------------------------
 function ToolboxUpdate(toolboxName)
 
-%% Setup functions
+% Setup functions
 ToolboxVer = str2func( sprintf('%sToolboxVer',toolboxName) );
 installToolbox = str2func( sprintf('install%sToolbox',toolboxName) );
 
-%% Check current version
+% Check current version
 try
     A = ToolboxVer;
 catch ME
@@ -28,7 +30,7 @@ catch ME
     fprintf('No previous version of %s detected.\n',toolboxName);
 end
 
-%% Setup temporary file directory
+% Setup temporary file directory
 fprintf('Downloading the %s Toolbox...',toolboxName);
 tmpFolder = sprintf('%sToolbox',toolboxName);
 pname = fullfile(tempdir,tmpFolder);
@@ -39,10 +41,8 @@ end
 % Create new directory
 [ok,msg] = mkdir(tempdir,tmpFolder);
 
-%% Download and unzip toolbox (GitHub)
-%url = sprintf('https://github.com/kutzer/%sToolbox/archive/master.zip',toolboxName); <--- Github removed references to "master"
-url = sprintf('https://github.com/kutzer/%sToolbox/archive/refs/heads/main.zip',toolboxName);
-
+% Download and unzip toolbox (GitHub)
+url = sprintf('https://github.com/kutzer/%sToolbox/archive/master.zip',toolboxName);
 try
     %fnames = unzip(url,pname);
     %urlwrite(url,fullfile(pname,tmpFname));
@@ -59,7 +59,7 @@ catch ME
     fprintf(2,'ERROR MESSAGE:\n\t%s\n',ME.message);
 end
 
-%% Check for successful download
+% Check for successful download
 alternativeInstallMsg = [...
     sprintf('Manually download the %s Toolbox using the following link:\n',toolboxName),...
     newline,...
@@ -79,28 +79,52 @@ if ~confirm
     return
 end
 
-%% Find base directory
+% Find base directory
 install_pos = strfind(fnames, sprintf('install%sToolbox.m',toolboxName) );
 sIdx = cell2mat( install_pos );
 cIdx = ~cell2mat( cellfun(@isempty,install_pos,'UniformOutput',0) );
 
 pname_star = fnames{cIdx}(1:sIdx-1);
 
-%% Get current directory and temporarily change path
+% Get current directory and temporarily change path
 cpath = cd;
 cd(pname_star);
 
-%% Install Toolbox
-installToolbox(true);
+% Check for admin
+skipAdmin = ~checkWriteAccess(matlabroot);
 
-%% Move back to current directory and remove temp file
+% Install Toolbox
+% TODO - consider providing the user with an option or more information
+%        related to "skipAdmin"
+try
+    installToolbox(true,skipAdmin);
+catch ME
+    cd(cpath);
+    throw(ME);
+end
+
+% Move back to current directory and remove temp file
 cd(cpath);
 [ok,msg] = rmdir(pname,'s');
 if ~ok
     warning('Unable to remove temporary download folder. %s',msg);
 end
 
-%% Complete installation
+% Complete installation
 fprintf('Installation complete.\n');
+
+end
+% -------------------------------------------------------------------------
+function tfWrite = checkWriteAccess(pname)
+
+tmpFname = fullfile(pname,'tmp.txt');
+tmpHndle = fopen(tmpFname, 'w');
+if tmpHndle < 0
+    tfWrite = false;
+else
+    tfWrite = true;
+    fclose(tmpHndle);
+    delete(tmpFname);
+end
 
 end
